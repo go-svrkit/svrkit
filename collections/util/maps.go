c7359b1ffab1701bb419b253e691c2c252426557
qiangchen <ichenq@outlook.com>
2024-03-01 23:11:09 +0800

    collections

---
+++ collections/util/maps.go
@@ -0,0 +1,69 @@
+// Copyright © Johnnie Chen ( ki7chen@github ). All rights reserved.
+// See accompanying files LICENSE.txt
+
+package util
+
+import (
+	"cmp"
+	"maps"
+	"slices"
+)
+
+// MapKeys 返回map的key列表
+func MapKeys[M ~map[K]V, K comparable, V any](m M) []K {
+	var keys = make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// MapValues 返回map的value列表
+func MapValues[M ~map[K]V, K comparable, V any](m M) []V {
+	var values = make([]V, 0, len(m))
+	for _, val := range m {
+		values = append(values, val)
+	}
+	return values
+}
+
+// MapKeyValues 返回map的key和value列表
+func MapKeyValues[M ~map[K]V, K comparable, V any](m M) ([]K, []V) {
+	var keys = make([]K, 0, len(m))
+	var values = make([]V, 0, len(m))
+	for k, val := range m {
+		keys = append(keys, k)
+		values = append(values, val)
+	}
+	return keys, values
+}
+
+// MapOrderedValues 返回map里按key排序的value列表
+func MapOrderedValues[M ~map[K]V, K cmp.Ordered, V any](m M) []V {
+	var keys = MapKeys(m)
+	slices.Sort(keys)
+	var values = make([]V, 0, len(m))
+	for _, k := range keys {
+		values = append(values, m[k])
+	}
+	return values
+}
+
+// MapUnion 返回两个map的并集, copy of a ∪ b
+func MapUnion[M ~map[K]V, K comparable, V any](a, b M) M {
+	var result = make(M, len(a)+len(b))
+	maps.Copy(result, a)
+	maps.Copy(result, b)
+	return result
+}
+
+// MapIntersect 返回两个map的交集, a ∩ b
+func MapIntersect[M ~map[K]V, K, V comparable](a, b M) M {
+	var result = make(map[K]V)
+	for k, v := range a {
+		if val, ok := b[k]; ok && val == v {
+			result[k] = v
+		}
+	}
+	return result
+}
---
+++ collections/util/maps_test.go
@@ -0,0 +1,124 @@
+package util
+
+import (
+	"fmt"
+	"maps"
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMapKeys(t *testing.T) {
+	tests := []struct {
+		m    map[int]int
+		want []int
+	}{
+		{map[int]int{}, []int{}},
+		{map[int]int{1: 1}, []int{1}},
+		{map[int]int{3: 3, 2: 2, 1: 1}, []int{3, 2, 1}},
+	}
+	for i, tt := range tests {
+		var name = fmt.Sprintf("case-%d", i+1)
+		t.Run(name, func(t *testing.T) {
+			var out = MapKeys(tt.m)
+			assert.True(t, slices.Equal(tt.want, out))
+		})
+	}
+}
+
+func TestMapValues(t *testing.T) {
+	tests := []struct {
+		m    map[int]string
+		want []string
+	}{
+		{map[int]string{}, []string{}},
+		{map[int]string{1: "a"}, []string{"a"}},
+		{map[int]string{1: "a", 2: "b", 3: "c"}, []string{"a", "b", "c"}},
+	}
+	for i, tt := range tests {
+		var name = fmt.Sprintf("case-%d", i+1)
+		t.Run(name, func(t *testing.T) {
+			var out = MapValues(tt.m)
+			assert.True(t, slices.Equal(tt.want, out))
+		})
+	}
+}
+
+func TestMapKeyValues(t *testing.T) {
+	tests := []struct {
+		m     map[int]string
+		want1 []int
+		want2 []string
+	}{
+		{map[int]string{}, nil, nil},
+		{map[int]string{1: "a"}, []int{1}, []string{"a"}},
+		{map[int]string{1: "a", 2: "b", 3: "c"}, []int{1, 2, 3}, []string{"a", "b", "c"}},
+	}
+	for i, tt := range tests {
+		var name = fmt.Sprintf("case-%d", i+1)
+		t.Run(name, func(t *testing.T) {
+			out1, out2 := MapKeyValues(tt.m)
+			assert.True(t, slices.Equal(tt.want1, out1))
+			assert.True(t, slices.Equal(tt.want2, out2))
+		})
+	}
+}
+
+func TestMapOrderedValues(t *testing.T) {
+	tests := []struct {
+		m    map[int]string
+		want []string
+	}{
+		{map[int]string{}, nil},
+		{map[int]string{1: "a"}, []string{"a"}},
+		{map[int]string{3: "a", 2: "b", 1: "c"}, []string{"c", "b", "a"}},
+	}
+	for i, tt := range tests {
+		var name = fmt.Sprintf("case-%d", i+1)
+		t.Run(name, func(t *testing.T) {
+			out := MapOrderedValues(tt.m)
+			assert.True(t, slices.Equal(tt.want, out))
+		})
+	}
+}
+
+func TestMapUnion(t *testing.T) {
+	tests := []struct {
+		m1   map[int]int
+		m2   map[int]int
+		want map[int]int
+	}{
+		{nil, nil, nil},
+		{map[int]int{1: 1}, nil, map[int]int{1: 1}},
+		{map[int]int{1: 1}, map[int]int{2: 2}, map[int]int{1: 1, 2: 2}},
+		{map[int]int{1: 10, 2: 20}, map[int]int{3: 30}, map[int]int{1: 10, 2: 20, 3: 30}},
+	}
+	for i, tt := range tests {
+		var name = fmt.Sprintf("case-%d", i+1)
+		t.Run(name, func(t *testing.T) {
+			var out = MapUnion(tt.m1, tt.m2)
+			assert.True(t, maps.Equal(tt.want, out))
+		})
+	}
+}
+
+func TestMapIntersect(t *testing.T) {
+	tests := []struct {
+		m1   map[int]string
+		m2   map[int]string
+		want map[int]string
+	}{
+		{nil, nil, nil},
+		{map[int]string{1: "a"}, nil, nil},
+		{map[int]string{1: "a"}, map[int]string{2: "b"}, nil},
+		{map[int]string{1: "a", 2: "b", 3: "c"}, map[int]string{1: "1", 2: "b", 3: "3"}, map[int]string{2: "b"}},
+	}
+	for i, tt := range tests {
+		var name = fmt.Sprintf("case-%d", i+1)
+		t.Run(name, func(t *testing.T) {
+			var out = MapIntersect(tt.m1, tt.m2)
+			assert.True(t, maps.Equal(tt.want, out))
+		})
+	}
+}
