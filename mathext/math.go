b52e83d777d42ef00f059e81027b473bdef4187e
qchen <ichenq@outlook.com>
2024-04-11 14:39:46 +0800

    mathext

--- mathext/math.go
+++ mathext/math.go
@@ -6,6 +6,7 @@ package mathext
 import (
 	"math"
 	"math/bits"
+	"math/rand"
 )
 
 func Max[T Integer](x, y T) T {
@@ -67,4 +68,104 @@ func SafeMulInt64(a, b int64) (product int64, overflow bool) {
 		return -int64(lo), false
 	}
 	return int64(lo), false
+}
+
+// LCG 线性同余法的随机数生成器
+// see https://en.wikipedia.org/wiki/Linear_congruential_generator
+type LCG struct {
+	seed uint32
+}
+
+func (g *LCG) Seed(seed uint32) {
+	g.seed = seed*214013 + 2531011
+}
+
+func (g *LCG) Rand() uint32 {
+	g.seed = g.seed*214013 + 2531011
+	var r = uint32(g.seed>>16) & 0x7fff
+	return r
+}
+
+// RandInt rand an integer in [min, max]
+func RandInt(min, max int) int {
+	if min > max {
+		panic("RandInt,min greater than max")
+	}
+	if min == max {
+		return min
+	}
+	return rand.Intn(max-min+1) + min
+}
+
+// RandFloat rand a float number in [min, max]
+func RandFloat(min, max float64) float64 {
+	if min > max {
+		panic("RandFloat: min greater than max")
+	}
+	if min == max {
+		return min
+	}
+	return rand.Float64()*(max-min) + min
+}
+
+// RangePerm [min,max]区间内的随机数集合
+func RangePerm(min, max int) []int {
+	if min > max {
+		panic("RangePerm: min greater than max")
+	}
+	if min == max {
+		return []int{min}
+	}
+	list := rand.Perm(max - min + 1)
+	for i := 0; i < len(list); i++ {
+		list[i] += min
+	}
+	return list
+}
+
+// Truncate 截断浮点数的`n`位后，n不应过大
+func Truncate(f float64, n int) float64 {
+	var x = math.Pow10(n)
+	return float64(int64(f*x)) / x
+}
+
+// RoundFloat round a float to a specific decimal place or precision
+// see https://github.com/montanaflynn/stats/blob/master/round.go
+func RoundFloat(x float64, places int) float64 {
+	// If the float is not a number
+	if math.IsNaN(x) {
+		return math.NaN()
+	}
+
+	// Find out the actual sign and correct the input for later
+	sign := 1.0
+	if x < 0 {
+		sign = -1
+		x *= -1
+	}
+
+	// Use the places arg to get the amount of precision wanted
+	precision := math.Pow(10, float64(places))
+
+	// Find the decimal place we are looking to round
+	digit := x * precision
+
+	// Get the actual decimal number as a fraction to be compared
+	_, decimal := math.Modf(digit)
+
+	// If the decimal is less than .5 we round down otherwise up
+	var rounded float64
+	if decimal >= 0.5 {
+		rounded = math.Ceil(digit)
+	} else {
+		rounded = math.Floor(digit)
+	}
+
+	// Finally we do the math to actually create a rounded number
+	return rounded / precision * sign
+}
+
+// RoundHalf 四舍五入
+func RoundHalf(v float64) int {
+	return int(RoundFloat(v, 0))
 }
--- mathext/math_test.go
+++ mathext/math_test.go
@@ -143,3 +143,145 @@ func BenchmarkSafeMulUint64(b *testing.B) {
 		SafeMulUint64(uint64(i), uint64(i))
 	}
 }
+
+func TestLazyLCGRand(t *testing.T) {
+	var rng LCG
+	for i := 0; i < 1000; i++ {
+		rng.Rand()
+	}
+}
+
+func TestSetLazyLCGSeed(t *testing.T) {
+	var rng LCG
+	rng.Seed(1234567890)
+	for i := 0; i < 1000; i++ {
+		rng.Rand()
+	}
+}
+
+func TestRandInt(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		v := RandInt(0, 1000)
+		if v < 0 {
+			t.Fatalf("%v < 0", v)
+		}
+		if v > 1000 {
+			t.Fatalf("%v > 1000", v)
+		}
+	}
+}
+
+func TestRandFloat(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		v := RandFloat(0, 1.0)
+		if v < 0 {
+			t.Fatalf("%v < 0.0", v)
+		}
+		if v > 1.0 {
+			t.Fatalf("%v > 1.0", v)
+		}
+	}
+}
+
+func TestRangePerm(t *testing.T) {
+	var appeared = make(map[int]bool, 1000)
+	var list = RangePerm(0, 1000)
+	for _, v := range list {
+		if v < 0 {
+			t.Fatalf("%v < 0", v)
+		}
+		if v > 1000 {
+			t.Fatalf("%v > 1000", v)
+		}
+		if _, found := appeared[v]; found {
+			t.Fatalf("duplicate %v", v)
+		}
+		appeared[v] = true
+	}
+}
+
+func TestMathTruncate(t *testing.T) {
+	var tests = []struct {
+		input     float64
+		precision int
+		expected  float64
+	}{
+		{1.2345678, 1, 1.2},
+		{1.2345678, 2, 1.23},
+		{1.2345678, 3, 1.234},
+		{-1.2345678, 1, -1.2},
+		{-1.2345678, 2, -1.23},
+		{-1.2345678, 3, -1.234},
+	}
+	for _, tc := range tests {
+		var output = Truncate(tc.input, tc.precision)
+		if output != tc.expected {
+			t.Fatalf("%f %d expected %f, but got %f", tc.input, tc.precision, tc.expected, output)
+		}
+	}
+}
+
+func TestRound(t *testing.T) {
+	for _, c := range []struct {
+		number   float64
+		decimals int
+		result   float64
+	}{
+		{0.1111, 1, 0.1},
+		{-0.1111, 2, -0.11},
+		{5.3253, 3, 5.325},
+		{5.3258, 3, 5.326},
+		{5.3253, 0, 5.0},
+		{5.55, 1, 5.6},
+	} {
+		m := RoundFloat(c.number, c.decimals)
+		if m != c.result {
+			t.Errorf("%.1f != %.1f", m, c.result)
+		}
+	}
+}
+
+func roundHalfSimple(f float64) float64 {
+	return math.Floor(f + 0.5)
+}
+
+func TestMathRoundHalf(t *testing.T) {
+	var roundTests = []struct {
+		input    float64
+		expected float64
+	}{
+		{-0.49999999999999994, -0.0}, // -0.5+epsilon
+		{-0.5, -1},
+		{-0.5000000000000001, -1}, // -0.5-epsilon
+		{0, 0},
+		{0.49999999999999994, 0}, // 0.5-epsilon
+		{0.5, 1},
+		{0.0, 0.0},
+		{0.5000000000000001, 1},  // 0.5+epsilon
+		{1.390671161567e-309, 0}, // denormal
+		{2.2517998136852485e+15, 2.251799813685249e+15}, // 1 bit fraction
+		{4.503599627370497e+15, 4.503599627370497e+15},  // large integer
+		{math.Inf(-1), math.Inf(-1)},
+		{math.Inf(1), math.Inf(1)},
+		{math.NaN(), math.NaN()},
+	}
+	for i, tc := range roundTests {
+		//var output = RoundFloat(tc.input, 0)
+		var output = roundHalfSimple(tc.input)
+		if math.IsNaN(output) {
+			if !math.IsNaN(tc.expected) {
+				t.Fatalf("%d: %f => %f != %f", i+1, tc.input, output, tc.expected)
+			}
+		} else {
+			if output != tc.expected {
+				t.Fatalf("%d: %f => %f != %f", i+1, tc.input, output, tc.expected)
+			}
+		}
+	}
+}
+
+func BenchmarkRound(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RoundFloat(0.1111, 1)
+	}
+}
