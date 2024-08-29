1e3dbf8cdce5e346b99059b6604a6662a2f6d5ca
qchen <ichenq@outlook.com>
2024-08-29 17:50:02 +0800

    arena

--- pool/arena.go
+++ pool/arena.go
@@ -5,35 +5,84 @@ package pool
 
 import (
 	"sync"
+	"unsafe"
 )
 
+// _PageSize at runtime/malloc.go
+const ArenaPageSize = 8192
+
 // ArenaPool
-// 一次申请一个block（N个元素的数组），然后从block数组里再逐个按需分配，
-// block分配完了就丢掉（交给GC)，再申请另一个block；
-// 这样对runtime来说每次malloc都是以N个元素大小的单位，可以减缓GC的压力
+// 一次申请一个block（N个元素的数组），然后从block数组里再逐个按需分配，试图减缓GC的压力
 type ArenaPool[T any] struct {
 	guard sync.Mutex
-	idx   int
+	off   int
 	block []T
 }
 
-func NewArenaPool[T any](blockSize int) *ArenaPool[T] {
+func NewArenaPool[T any]() *ArenaPool[T] {
+	var dummy T
+	var blockSize = ArenaPageSize / unsafe.Sizeof(dummy)
 	return &ArenaPool[T]{
 		block: make([]T, blockSize),
 	}
 }
 
+func NewArenaPoolWith[T any](blockSize int) *ArenaPool[T] {
+	return &ArenaPool[T]{
+		block: make([]T, blockSize),
+	}
+}
+
+// Size block的大小
+func (a *ArenaPool[T]) Size() int {
+	return len(a.block)
+}
+
+// Len 已分配的元素个数
+func (a *ArenaPool[T]) Len() int {
+	return a.off
+}
+
+// Cap 剩余可分配空间
+func (a *ArenaPool[T]) Cap() int {
+	return len(a.block) - a.off
+}
+
+// Ptr 返回block的指针
+func (a *ArenaPool[T]) Ptr() unsafe.Pointer {
+	return unsafe.Pointer(&a.block[0])
+}
+
+// Alloc 从block里分配一个
 func (a *ArenaPool[T]) Alloc() *T {
 	a.guard.Lock()
 	defer a.guard.Unlock()
 
 	var size = len(a.block)
-	var ret = &a.block[a.idx]
-	a.idx++
-	if a.idx >= size {
+	if size-a.off == 0 {
 		a.block = make([]T, size)
-		a.idx = 0
+		a.off = 0
 	}
+	var ret = &a.block[a.off]
+	a.off++
+	return ret
+}
+
+// AllocN 从block里分配n个
+func (a *ArenaPool[T]) AllocN(n int) []T {
+	a.guard.Lock()
+	defer a.guard.Unlock()
+
+	var size = len(a.block)
+	if size < n {
+		return nil // too big
+	}
+	if size-a.off < n {
+		a.block = make([]T, size)
+		a.off = 0
+	}
+	var ret = a.block[a.off : a.off+n]
+	a.off += n
 	return ret
 }
 
--- pool/arena_test.go
+++ pool/arena_test.go
@@ -8,27 +8,36 @@ import (
 	"context"
 	"testing"
 	"time"
-	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestArena_Simple(t *testing.T) {
-	var a = NewArenaPool[bytes.Buffer](100)
+	var a = NewArenaPool[int]()
+	assert.Equal(t, a.Len(), 0)
+	assert.Equal(t, a.Cap(), 1024)
+	assert.Equal(t, a.Size(), 1024)
+
 	var b = a.Alloc()
 	assert.NotNil(t, b)
+	assert.Equal(t, a.Len(), 1)
+	assert.Equal(t, a.Cap(), 1023)
+	assert.Equal(t, a.Size(), 1024)
 
-	a = NewArenaPool[bytes.Buffer](1)
-	b = a.Alloc()
-	assert.NotNil(t, b) // exhaust the block
-	var idx = a.idx
-	a.Free(b)
-	assert.Equal(t, idx, a.idx)
-	var ptr = unsafe.Pointer(&a.block[0])
+	var c = a.AllocN(23)
+	assert.NotNil(t, c)
+	assert.Equal(t, len(c), 23)
+	assert.Equal(t, a.Len(), 24)
+	assert.Equal(t, a.Cap(), 1000)
+	assert.Equal(t, a.Size(), 1024)
 
-	b = a.Alloc() // should allocate a new block
-	assert.NotNil(t, b)
-	assert.NotEqual(t, ptr, unsafe.Pointer(&a.block[0]))
+	var oldPtr = a.Ptr()
+	var capacity = a.Cap()
+	for i := 0; i < capacity; i++ {
+		a.Alloc()
+	}
+	a.Alloc() // alloc new block
+	assert.NotEqual(t, oldPtr, a.Ptr())
 }
 
 func arenaWorker(ctx context.Context, t *testing.T, pool *ArenaPool[bytes.Buffer]) {
@@ -53,7 +62,7 @@ func TestArena_Concurrent(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1500*time.Millisecond)
 	defer cancel()
 
-	var a = NewArenaPool[bytes.Buffer](100)
+	var a = NewArenaPool[bytes.Buffer]()
 	for i := 0; i < 10; i++ {
 		go arenaWorker(ctx, t, a)
 	}
